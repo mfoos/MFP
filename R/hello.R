311db2847bca718f295a62ce5e8a403eaeef04ee
Marianna Foos <marianna.foos@biogen.com>
2017-07-26 11:31:46 -0400

    v meaningful message

--- R/hello.R
+++ R/hello.R
@@ -1,3 +1,13 @@
+#' @aliases greet
+#' @aliases hello
+#' @title Greeting a Person
+#' @description Given a person's name, a custom greeting is printed
+#' @rdname hello
+#' @param name character() Name of person
+#' @return character() Greeting
+#' @examples
+#' greet("Mary")
+#' @export
 greet <- function(name){
   print(paste("Hello", name))
 }
